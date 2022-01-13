a6ff6b723e3c8459967027bd921116fdeec27265
IITvamp <ayush_b@ece.iitr.ac.in>
2022-01-13 19:33:46 +0530

    adding Procfile

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.port || 8011;
+const PORT = process.env.PORT || 8011;
 
 const mongoose = require("mongoose");
 mongoose
